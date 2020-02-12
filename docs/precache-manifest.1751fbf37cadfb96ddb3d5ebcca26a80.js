5010be489aee66afbbd3a098a6d2cd7fdfc113e0
Kailey <kale@Kales-Mac.local>
2020-02-12 10:19:01 -0800

    Portfolio project files

---
+++ docs/precache-manifest.1751fbf37cadfb96ddb3d5ebcca26a80.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de998aba31782d2e929678834a7efea0",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "38a2d1abbb7fe71f5945",
+    "url": "/portfolio/static/css/main.c350ee45.chunk.css"
+  },
+  {
+    "revision": "f686e99a9b6a6c927b70",
+    "url": "/portfolio/static/js/2.4afbc96e.chunk.js"
+  },
+  {
+    "revision": "f86a4e17d3cb58f717d9f416937db9aa",
+    "url": "/portfolio/static/js/2.4afbc96e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38a2d1abbb7fe71f5945",
+    "url": "/portfolio/static/js/main.e1be2375.chunk.js"
+  },
+  {
+    "revision": "0d98471f6c3b69ca4251",
+    "url": "/portfolio/static/js/runtime-main.44087e9b.js"
+  },
+  {
+    "revision": "4317be933b8b615a4b7847d40093ae3f",
+    "url": "/portfolio/static/media/header.4317be93.jpg"
+  }
+]);
---
+++ src/resumeData.js
@@ -0,0 +1,101 @@
+let resumeData = {
+  name: "Kailey Terracciano",
+  role: "UX Designer",
+  linkedinId:"kaileytea",
+  skypeid: "kaileytea",
+  roleDescription: "I love to research and create narratives and design based on those findings.",
+  socialLinks:[
+      {
+        name:"linkedin",
+        url:"https://www.linkedin.com/in/kaileytea",
+        className:"fa fa-linkedin"
+      },
+      {
+        name:"github",
+        url:"http://github.com/kaileyteas",
+        className:"fa fa-github"
+      },
+      {
+        name:"skype",
+        url:"http://skype.com",
+        className:"fa fa-skype"
+      }
+    ],
+  aboutme:"I'm a junior at UW and am currently seeking a full time internship over the Summer.",
+  address:"Seattle, Washington",
+  education:[
+    {
+      UniversityName:"University of Washington",
+      specialization:"B.S. Human Centered Design & Engineering",
+      MonthOfPassing:"June",
+      YearOfPassing:"2021",
+      Achievements:"Some Achievements"
+    },
+    {
+      UniversityName:"Some University",
+      specialization:"Some specialization",
+      MonthOfPassing:"Jan",
+      YearOfPassing:"2018",
+      Achievements:"Some Achievements"
+    }
+  ],
+  work:[
+    {
+      CompanyName:"UW ",
+      specialization:"Instructor",
+      MonthOfLeaving:"Jan",
+      YearOfLeaving:"2018",
+      Achievements:"Some Achievements"
+    },
+    {
+      CompanyName:"Some Company",
+      specialization:"Some specialization",
+      MonthOfLeaving:"Jan",
+      YearOfLeaving:"2018",
+      Achievements:"Some Achievements"
+    }
+  ],
+  skillsDescription:"My skills:",
+  skills:[
+    {
+      skillname:"Reactjs",
+      level:90,
+    },
+    {
+      skillname:"HTML5",
+      level:70,
+    },
+    {
+      skillname:"CSS",
+      level: 80,
+    },
+  ],
+  portfolio:[
+    {
+      name:"project1",
+      description:"mobileapp",
+      imgurl:"images/portfolio/phone.jpg",
+      title:"Phone App",
+      githubLink:"https://github.com/kaileyteas/chatapp2020",
+      fullDescription:"this is a longer description about this app this is a longer description about this app this is a longer description about this app this is a longer description about this app this is a longer description about this app this is a longer description about this app this is a longer description about this app "
+    },
+    {
+      name:"asdfasdfasf",
+      description:"mobileapp",
+      imgurl:"images/portfolio/weatherer.png",
+      title:"Something",
+      githubLink:"https://github.com",
+      fullDescription:"this is a longer description about this app"
+    },
+    {
+      name:"project3",
+      description:"mobileapp",
+      imgurl:"images/portfolio/chatter.jpg",
+      title:"Hello",
+      githubLink:"https://github.com",
+      fullDescription:"this is a longer description about this app"
+    },
+  ],
+}
+
+export default resumeData
